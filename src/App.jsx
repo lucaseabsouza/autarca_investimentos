afd1dccfeb5f8b1e1d38b728956f3eaceadf8bb4
lucaseabsouza <lucaseabsouza@gmail.com>
2024-07-08 21:18:43 -0300

    Initial Commit
    Configuring the components, folders, basic styles css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
-      <h1>Autarca Investimentos</h1>
+      <Header/>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+
+  return (
+    <>
+      <img className='logo_header' src='../public/imgs/logo_bg_black.png'></img>
+    </>
+  )
+}
+
+export default Header
