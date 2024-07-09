bb363732302ab48339e501530fdcdd480a9dd6c7
lucaseabsouza <lucaseabsouza@gmail.com>
2024-07-08 21:03:11 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# autarca_investimentos
+ A plataform to manage your investments
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1>Autarca Investimentos</h1>
+    </>
+  )
+}
+
+export default App
