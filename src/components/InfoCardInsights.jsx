5e460ee82002d95280867b515f24a135ddbf4ec1
lucaseabsouza <lucaseabsouza@gmail.com>
2024-07-10 21:33:56 -0300

    Putting prefix in values

--- src/components/InfoCardInsights.jsx
+++ src/components/InfoCardInsights.jsx
@@ -2,14 +2,14 @@ const infoCardInsights = [
     {
         id: 1,
         titleCardInsights: 'Patrimônio',
-        valueCardInsights: 45678.90,
+        valueCardInsights: 'R$ ' + 45678.90,
         detailsCardInsights: '+20% nos últimos 12 meses',
     },
 
     {
         id: 2,
         titleCardInsights: 'Proventos',
-        valueCardInsights: 25,
+        valueCardInsights: 'R$ ' + 25,
         detailsCardInsights: '+33% nos últimos 12 meses',
     },
 ]
