1f72dee1ca922397961a5b985f817547c31be786
lucaseabsouza <lucaseabsouza@gmail.com>
2024-07-21 20:39:48 -0300

    Simple Sets

--- src/components/CardProfitability.jsx
+++ src/components/CardProfitability.jsx
@@ -10,13 +10,13 @@ export default function CardProfitability() {
                 <p className='insightProfitability'>+5,26%</p>
                 <button className='btnSelect'>
                     Período
-                    <svg style={{margin: "0 0 0 1rem"}} width="1rem" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="#ffffff" d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg>
+                    <svg style={{margin: "0 0 0 1rem"}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="#ffffff" d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg>
                 </button>
                 <button className='btnSelect'>
                     Ativos
-                    <svg style={{margin: "0 0 0 1rem"}} width="1rem" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="#ffffff" d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg>
+                    <svg style={{margin: "0 0 0 1rem"}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="#ffffff" d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg>
                 </button>
-                <ChartLineProfitability/>
+                <ChartLineProfitability />
             </div>
         </>
     )
--- src/components/InfoCardInsights.jsx
+++ src/components/InfoCardInsights.jsx
@@ -2,14 +2,14 @@ const infoCardInsights = [
     {
         id: 1,
         titleCardInsights: 'Patrimônio',
-        valueCardInsights: 'R$ ' + 45678.90,
+        valueCardInsights: 'R$ ' + "45.678,90",
         detailsCardInsights: '+20% nos últimos 12 meses',
     },
 
     {
         id: 2,
         titleCardInsights: 'Proventos',
-        valueCardInsights: 'R$ ' + 25,
+        valueCardInsights: 'R$ ' + "25,00",
         detailsCardInsights: '+33% nos últimos 12 meses',
     },
 ]
