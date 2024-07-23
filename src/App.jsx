158f9738b28d6e8f60b348caa9bd20517e57a7c3
lucaseabsouza <lucaseabsouza@gmail.com>
2024-07-23 19:53:16 -0300

    Create prevent route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import PageProfitability from './components/PageProfitability';
 import PageWallet from './components/PageWallet';
 import PageProfits from './components/PageProfits';
@@ -10,6 +10,7 @@ export default function App() {
   return (
     <>
       <Routes>
+        <Route path='*' element={ <Navigate to='/' /> }></Route>
         <Route path='/' element={ <PageProfitability/> } />
         <Route path='/carteira' element={ <PageWallet/> } />
         <Route path='/proventos' element={ <PageProfits/> } />
