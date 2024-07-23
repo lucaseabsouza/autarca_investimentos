cfc785500c432b672dacb8a7ffae59e58da4e5d9
lucaseabsouza <lucaseabsouza@gmail.com>
2024-07-23 20:12:36 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import reactRefresh from '@vitejs/plugin-react-refresh'
-
-const root = resolver(__dirname, 'src')
-const outDir = resolve(__dirname, 'dist')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root,
-  plugins: [reactRefresh()],
-  build : {
-    outDir,
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(root, 'main.jsx'),
-        wallet: resolve(root, 'PageWallet.jsx', 'main,jsx'),
-      }
-    }
-  }
+  plugins: [react()],
 })
