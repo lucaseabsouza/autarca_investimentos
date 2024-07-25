065a73c08412007c8a7b2f4a9844287d4cd67047
lucaseabsouza <lucaseabsouza@gmail.com>
2024-07-25 20:00:22 -0300

    Update PageProfitability.jsx

--- src/components/PageProfitability.jsx
+++ src/components/PageProfitability.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from './Header'
 import CardPatrimony from "./CardPatrimony";
 import CardProfitability from "./CardProfitability";
+import { Link } from 'react-router-dom';
 
 export default function PageProfitability() {
     return (
@@ -9,5 +10,6 @@ export default function PageProfitability() {
             <Header/>
             <CardPatrimony/>
             <CardProfitability/>
+            <Link to='/carteira'>Carteira</Link>
         </>
     )}
