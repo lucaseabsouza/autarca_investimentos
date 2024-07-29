a1b0b85d604542b2414f6d5721c4756aaa425552
lucaseabsouza <lucaseabsouza@gmail.com>
2024-07-29 20:59:37 -0300

    Testing Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, BrowserRouter as Router, Navigate } from 'react-router-dom';
 import PageProfitability from './components/PageProfitability';
 import PageWallet from './components/PageWallet';
 import PageProfits from './components/PageProfits';
@@ -8,13 +8,13 @@ import PageProfits from './components/PageProfits';
 export default function App() {
 
   return (
-    <>
+    <Router>
       <Routes>
         <Route path='*' element={ <Navigate to='/' /> }></Route>
         <Route path='/' element={ <PageProfitability/> } />
         <Route path='/carteira' element={ <PageWallet/> } />
         <Route path='/proventos' element={ <PageProfits/> } />
       </Routes>
-    </>
+    </Router>
   )
 }
