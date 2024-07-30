8ed20b2bfd67458523bf8fe79bcce678e2c8640f
lucaseabsouza <lucaseabsouza@gmail.com>
2024-07-30 20:15:02 -0300

    Create Profits Doughnut Chart

---
+++ src/components/ChartDoughnut.jsx
@@ -0,0 +1,51 @@
+import { Doughnut } from 'react-chartjs-2'
+import {
+    Chart as ChartJS,
+    ArcElement,
+    Tooltip,
+    Legend
+    } from 'chart.js';
+
+import ChartDataLabels from 'chartjs-plugin-datalabels'
+import { color } from 'chart.js/helpers';
+
+    ChartJS.register(
+        ArcElement,
+        Tooltip,
+        Legend,
+        ChartDataLabels
+    )
+
+export const ChartDoughnutProfits = () => {
+    const DataChartDoughnutProfits = {
+        labels: [
+            'Ações',
+            'FIIs',
+        ],
+        datasets: [
+            {   
+                data: [2560, 1952],
+                backgroundColor: [
+                    '#174ea1',
+                    '#586f92',
+                ],
+                borderWidth: 5,
+                borderColor: "#EAEAEA",
+                borderRadius: 8,
+                hoverBackgroundColor: "#EAEAEA",
+                hoverBorderWidth: 5,
+                spacing: 10
+            },
+        ],
+    };
+    const options = {
+        responsive: true,
+        plugins: {
+            datalabels: {
+                display: true,
+                color: "#fff",
+            },
+        },
+    }
+    return <Doughnut className="chartdoughnutprofits" options={options} data={DataChartDoughnutProfits} />
+}
