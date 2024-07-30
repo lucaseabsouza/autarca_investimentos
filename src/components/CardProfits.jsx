5ce60f41037a4a5350b4756874455d9e64eadb94
lucaseabsouza <lucaseabsouza@gmail.com>
2024-07-30 19:24:13 -0300

    Create page Profits
    Create Page, Chart and Card Profits

--- src/components/CardProfits.jsx
+++ src/components/CardProfits.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { ChartLineProfitability } from './ChartLineProfitability';
+import { ChartLineProfits } from './ChartLineProfits';
 
-export default function CardProfitability() {
+export default function CardProfits() {
     return (
         <>
             <div className='containerCards'>
-                <h1 className='titles'>Rentabilidade</h1>
-                <h2 className='insights'>R$ 11.325,60</h2>
-                <p className='insights'>+5,26%</p>
+                <h1 className='titles'>Proventos</h1>
+                <h2 className='insights'>R$ 260,38</h2>
                 <div className="containerBtnSelect">
                     <button className='btnSelect'>
                         Período
@@ -18,7 +17,7 @@ export default function CardProfitability() {
                         <svg style={{margin: "0 0 0 1rem", width: "1rem"}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="#00" d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg>
                     </button>
                 </div>
-                <ChartLineProfitability />
+                <ChartLineProfits />
             </div>
         </>
     )
--- src/components/PageProfits.jsx
+++ src/components/PageProfits.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from './Header'
 import Footer from "./Footer";
+import CardProfits from "./CardProfits";
 
 export default function PageProfits() {
     return (
         <>
             <Header/>
-            <h1 className="titles">Proventos</h1>
+            <CardProfits></CardProfits>
             <Footer></Footer>
         </>
     )
