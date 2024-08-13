4a58bf128f31f323f30086acd248c1fe2fb6fd14
lucaseabsouza <lucaseabsouza@gmail.com>
2024-08-13 19:52:09 -0300

    Create function to scroll to top
    Create function to scroll to top when change page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import InitialPage from './components/InitialPage';
 import WalletPage from './components/WalletPage';
 import ProfitsPage from './components/ProfitsPage';
@@ -8,12 +8,12 @@ import NotificationsPage from './components/NotificationsPage';
 export default function App() {
 
   return (
-      <Routes>
-        <Route path='*' element={ <Navigate to='/' /> }/>
-        <Route path='/' element={ <InitialPage/> } />
-        <Route path='/carteira' element={ <WalletPage/> } />
-        <Route path='/proventos' element={ <ProfitsPage/> } />
-        <Route path='/notificacoes' element={ <NotificationsPage/> } />
-      </Routes>
+    <Routes>
+      <Route path='*' element={ <Navigate to='/' /> }/>
+      <Route path='/' element={ <InitialPage/> } />
+      <Route path='/carteira' element={ <WalletPage/> } />
+      <Route path='/proventos' element={ <ProfitsPage/> } />
+      <Route path='/notificacoes' element={ <NotificationsPage/> } />
+    </Routes>
   )
 }
